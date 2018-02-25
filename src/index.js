54380fe62ba34e03707273c91284065b90032ac6
Bobarykinvl <bobarykinvl@gmail.com>
2018-02-25 22:23:42 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+	
+  	var count_zero = 0;
+	while(number){
+		number = Math.floor(number / 5);
+		count_zero += number;
+		console.log('number:  ' + number);
+		console.log('count_zero:  ' + count_zero);
+		
+	}
+
+	
+	return count_zero;	
+		
 }
